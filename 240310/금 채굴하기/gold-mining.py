8fb5a1ff4c4a5b099e03fc6f0203e384ee14ca89
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-03-10 03:05:02 +0900

    Title: 금 채굴하기, Time: 269ms, Memory: 27MB, Status: Passed - CodeTree

--- 240310/금 채굴하기/gold-mining.py
+++ 240310/금 채굴하기/gold-mining.py
@@ -1,25 +1,23 @@
-hy = [0, 1, 0, -1]
-hx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+dx = [1, 0, -1, 0]
 
-dy = [1, 1, -1, -1]
-dx = [1, -1, -1, 1]
+dl = [(1, -1), (-1, -1), (-1, 1), (1, 1)]
 
 
-def search(n, m, check):
+def point_set(n, m, length):
+    for k in range(4):
+        ny, nx = n + length*dy[k], m + length*dx[k]
+        point_lst.append([k, ny, nx])
+
+
+def search(p_lst, length):
     count = 0
-    for hc in range(1, check + 1):
-        for hk in range(4):
-            hny, hnx = n + hc * hy[hk], m + hc * hx[hk]
-            if 0 <= hny < N and 0 <= hnx < N:
-                if arr[hny][hnx] == 1:
-                    count += 1
-
-    for dc in range(1, check):
-        for dk in range(4):
-            dny, dnx = n + dc * dy[dk], m + dc * dx[dk]
-            if 0 <= dny < N and 0 <= dnx < N:
-                if arr[dny][dnx] == 1:
-                    count += 1
+    while p_lst:
+        d, n, m = p_lst.pop()
+        for l in range(length):
+            ny, nx = n + l*dl[d][0], m + l*dl[d][1]
+            if 0 <= ny < N and 0 <= nx < N:
+                count += arr[ny][nx]
     return count
 
 
@@ -27,17 +25,19 @@ N, M = map(int, input().split())
 
 arr = [list(map(int, input().split())) for _ in range(N)]
 
-
 max_result = 0
 for i in range(N):
     for j in range(N):
-        for p in range(2 * (N - 1) + 1):
-            cost = p * p + (p + 1) * (p + 1)
-            gold = 0
-            if arr[i][j] == 1:
-                gold += 1
-            gold += search(i, j, p)
-            if cost <= gold * M:
+        gold = arr[i][j]
+        for p in range(2*(N-1) + 1):
+            cost = p*p + (p+1)*(p+1)
+            if p != 0:
+                point_lst = []
+                point_set(i, j, p)
+
+                gold += search(point_lst, p)
+
+            if cost <= gold*M:
                 max_result = max(max_result, gold)
 
 print(max_result)
