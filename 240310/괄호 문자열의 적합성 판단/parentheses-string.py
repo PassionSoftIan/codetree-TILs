8843b3bf76defe7c554c0a11a722155fb8fcce62
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-03-10 05:35:15 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240310/괄호 문자열의 적합성 판단/parentheses-string.py
@@ -0,0 +1,16 @@
+arr = list(map(str, input()))
+
+count = 0
+
+while arr:
+    check = arr.pop()
+    if check == ')':
+        count += 1
+    else:
+        if count == 0:
+            print('No')
+            break
+        else:
+            count = 0
+else:
+    print('Yes')
