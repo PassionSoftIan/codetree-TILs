fdf1bd64fc32a059fb387349cdd26ab67fc9cfa4
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-03-10 01:49:34 +0900

    Title: 금 채굴하기, Time: 124ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240310/금 채굴하기/gold-mining.py
+++ 240310/금 채굴하기/gold-mining.py
@@ -1,38 +1,43 @@
-dy = [0, 1, 0, -1]
-dx = [1, 0, -1, 0]
+hy = [0, 1, 0, -1]
+hx = [1, 0, -1, 0]
+
+dy = [1, 1, -1, -1]
+dx = [1, -1, -1, 1]
 
 
-def search(depth, end, n, m):
-    global count
+def search(n, m, check):
+    count = 0
+    for hc in range(1, check + 1):
+        for hk in range(4):
+            hny, hnx = n + hc * hy[hk], m + hc * hx[hk]
+            if 0 <= hny < N and 0 <= hnx < N:
+                if arr[hny][hnx] == 1:
+                    count += 1
 
-    if depth == end:
-        return
-
-    for k in range(4):
-        ny, nx = n + dy[k], m + dx[k]
-        if 0 <= ny < N and 0 <= nx < N and visited[ny][nx] == 0:
-            visited[ny][nx] = 1
-            if arr[ny][nx] == 1:
-                count += 1
-        search(depth + 1, end, ny, nx)
+    for dc in range(1, check):
+        for dk in range(4):
+            dny, dnx = n + dc * dy[dk], m + dc * dx[dk]
+            if 0 <= dny < N and 0 <= dnx < N:
+                if arr[dny][dnx] == 1:
+                    count += 1
+    return count
 
 
 N, M = map(int, input().split())
 
 arr = [list(map(int, input().split())) for _ in range(N)]
 
+
 max_result = 0
 for i in range(N):
     for j in range(N):
-        for p in range(12):
-            visited = [[0] * N for _ in range(N)]
-            visited[i][j] = 1
+        for p in range(2 * (N - 1) + 1):
             cost = p * p + (p + 1) * (p + 1)
-            count = 0
+            gold = 0
             if arr[i][j] == 1:
-                count += 1
-            search(0, p, i, j)
-            if cost <= count * M:
-                max_result = max(max_result, count)
+                gold += 1
+            gold += search(i, j, p)
+            if cost <= gold * M:
+                max_result = max(max_result, gold)
 
 print(max_result)
