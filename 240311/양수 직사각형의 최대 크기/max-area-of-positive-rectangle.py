64a701719ea76955fbd62dbe7d5291c5e3c642e7
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-03-11 00:34:03 +0900

    Title: 양수 직사각형의 최대 크기, Time: 124ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240311/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -0,0 +1,56 @@
+def check_col(n, m, row, col):
+    count = 0
+    for k in range(row):
+        temp = 0
+        for p in range(col):
+            if 0 < arr[n+k][m+p]:
+                temp += 1
+            else:
+                return count
+        count += temp
+    return count
+
+def check_row(n, m, row, col):    
+    count = 0
+    for k in range(col):
+        temp = 0
+        for p in range(row):
+            if 0 < arr[n+p][m+k]:
+                temp += 1
+            else:
+                return count
+        count += temp
+    return count
+
+def col(n, m):
+    count = 0
+    for k in range(m, M):
+        if 0 < arr[n][k]:
+            count += 1
+        else:
+            return count
+    return count
+
+def row(n, m):
+    count = 0
+    for k in range(n, N):
+        if 0 < arr[k][m]:
+            count += 1
+        else:
+            return count
+    return count
+
+N, M = map(int, input().split())
+
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+max_result = -1
+for i in range(N):
+    for j in range(M):
+        if 0 < arr[i][j]:
+            col_count = col(i, j)
+            row_count = row(i, j)
+            max_result = max(max_result, check_row(i, j, row_count, col_count), check_col(i, j, row_count, col_count))
+
+
+print(max_result)
