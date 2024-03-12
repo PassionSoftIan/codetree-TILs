2e155fcbc78b3c962febebaec790802a012daafc
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-03-12 11:51:30 +0900

    Title: 겹치지 않는 선분 2, Time: 159ms, Memory: 24MB, Status: Passed - CodeTree

--- 240312/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 240312/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -5,14 +5,13 @@
 2) 기준점과 비교값이 동일하지 않은 경우 탐색을 실시한다.
 3-1) 비교값이 만약 x1 보다는 크고 x2 보다는 작은 경우라면 개수를 올려준 뒤 반복문을 빠져나온다.
 3-2) 비교값이 만약 x1 보다 작고 x2 보다는 큰 경우라면 개수를 올려준 뒤 반복문을 빠져나온다.
-4) 최종 값을 결과로 출력한다.
+4) 선분의 전체 개수에서 겹치는 선분의 개수를 빼준다.
+5) 최종 값을 결과로 출력한다.
 '''
 
 N = int(input())
 
 arr = [list(map(int, input().split())) for _ in range(N)]
-
-visited = [0] * (N+1)
 
 result = 0
 for i in range(N):
@@ -20,7 +19,7 @@ for i in range(N):
     x1, x2 = arr[i][0], arr[i][1]
     
     for j in range(N):
-        if i == j or visited[j] == 1:
+        if i == j:
             continue
     
         x3, x4 = arr[j][0], arr[j][1]
@@ -29,4 +28,4 @@ for i in range(N):
             result += 1
             break
 
-print(result)
+print(N - result)
