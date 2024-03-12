66ca3f24100b94f4c54b841f79a2d7fc9b9b48ab
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-03-12 23:30:49 +0900

    Title: 이상한 폭탄 2, Time: 120ms, Memory: 25MB, Status: Passed - CodeTree

--- 240312/이상한 폭탄 2/strange-bomb-2.py
+++ 240312/이상한 폭탄 2/strange-bomb-2.py
@@ -9,8 +9,7 @@
 2) 지정한 폭탄과 같은 번호를 가진 폭탄을 찾는다.
 3) 지정한 폭탄과 같은 번호를 가진 폭탄의 거리를 구한다.
 4) 거리를 비교하여 K보다 작으면 터지는 폭탄으로 규정한다.
-5) 터지는 폭탄을 포함하여 사이에 모든 폭탄들을 탐색한다.
-6) 번호가 가장 큰 폭탄의 번호와 비교해서 번호가 큰 것으로 최대 폭탄의 번호를 교체한다.
+5) 터지는 폭탄과 현재 최대 폭탄 번호와 비교하여 큰 번호로 교체한다.
 7) 최대 폭탄의 번호를 출력한다.
 '''
 
@@ -21,9 +20,10 @@ arr = [int(input()) for _ in range(N)]
 max_result = -1
 for i in range(N):
     for j in range(N):
+        if i == j:
+            continue
         if arr[i] == arr[j]:        
             if abs(i - j) <= K:
-                for k in range(i, j):
-                    max_result = max(max_result, arr[k])
+                max_result = max(max_result, arr[i])
 
 print(max_result)
