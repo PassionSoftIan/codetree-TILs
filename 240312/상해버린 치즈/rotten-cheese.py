107e5b81f2509692042ad9bbdcb24f28667d6f22
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-03-12 21:39:49 +0900

    Title: 상해버린 치즈, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240312/상해버린 치즈/rotten-cheese.py
+++ 240312/상해버린 치즈/rotten-cheese.py
@@ -16,13 +16,13 @@ sick = 몇 번째 사람(p), 몇초에 확실히 아팠는지(t초)
 2) 아픈 기록 만큼 순회하며 1번에서 순회중인 사람이 명단에 있는지 체크한다.
 3) 명단에 있으면 알약 개수를 올리고 아픈 사람으로 체크를 한 뒤 언제부터 아팠는지 저장한다.
 4) 치즈 개수 + 1 만큼 0인 배열을 만든다(가장 바깥에)
-5) 아픈 사람이 먹은 치즈를 치즈 배열에 +1을 한다
+5) 아픈 사람이 먹은 치즈를 치즈 배열에 +1을 한다. (중복을 체크한다)
 6) 다시 치즈를 먹은 기록을 돈다.
 7) 해당 사람이 아픈 사람으로 체크된적이 있는지 본다.
 8) 체크된 적이 없다면 해당 사람이 먹은 치즈를 확인하는데 해당 치즈 인덱스에 숫자가
 아픈사람의 숫자와 일치한지 체크한다.
 9) 체크한 뒤 일치한다면 알약 수를 늘린다.
-9) 알약 수를 제출한다
+9) 알약 수를 제출한다.
 
 '''
 
@@ -45,16 +45,16 @@ for i in range(1, N + 1):
         if i == sick[j][0]:
             sick_time = sick[j][1]
             pill += 1
-            human[i] = 1
         else:
             continue
         for k in range(D):
-            if eat[k][0] != i:
+            if eat[k][0] != i or human[i] == 1:
                 continue
             cheese_eat_time = eat[k][2]
             cheese_num = eat[k][1]
             if sick_time > cheese_eat_time:
                 cheese[cheese_num] += 1
+                human[i] = 1
 
 
 for i in range(D):
