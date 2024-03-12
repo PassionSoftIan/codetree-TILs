2ca9075aee122857f6f46a3feeae74a74db849c7
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-03-12 21:03:07 +0900

    Title: 상해버린 치즈, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240312/상해버린 치즈/rotten-cheese.py
+++ 240312/상해버린 치즈/rotten-cheese.py
@@ -14,7 +14,7 @@ sick = 몇 번째 사람(p), 몇초에 확실히 아팠는지(t초)
 3) 명단에 있으면 알약 개수를 올리고 아픈 사람으로 체크를 한 뒤 언제부터 아팠는지 저장한다.
 4) 치즈 개수 + 1 만큼 0인 배열을 만든다(가장 바깥에)
 5) 치즈를 먹은 기록을 순회하며 저장해둔 아픈 시점보다 치즈를 먹은 시점이 더 이전이면
-먹은 치즈의 번호를 가지고 치즈 배열에 저장된 치즈를 먹은 시점과 비교하여 더 이른 시점인 것을 기록한다.
+먹은 치즈의 번호를 가지고 치즈 배열에 저장된 치즈를 먹은 시점과 비교하여 더 늦은 시점인 것을 기록한다.
 6) 치즈 기록을 돈다.
 7) 해당 사람이 아픈 사람으로 체크된적이 있는지 본다.
 8) 체크된 적이 없다면 해당 사람이 치즈를 먹은 시점을 체크한다.
@@ -30,7 +30,7 @@ eat = [list(map(int, input().split())) for _ in range(D)]
 
 sick = [list(map(int, input().split())) for _ in range(S)]
 
-cheese = [101] * (M+1)
+cheese = [0] * (M+1)
 
 human = [0] * (N+1)
 
@@ -51,14 +51,14 @@ for i in range(1, N + 1):
             cheese_eat_time = eat[k][2]
             if sick_time > cheese_eat_time:
                 cheese_num = eat[k][1]
-                cheese[cheese_num] = min(cheese[cheese_num], eat[k][2])
+                cheese[cheese_num] = max(cheese[cheese_num], eat[k][2])
 
 
 for i in range(D):
     human_num = eat[i][0]
     cheese_num = eat[i][1]
     eat_time = eat[i][2]
-    if human[human_num] == 0 and cheese[cheese_num] < eat_time:
+    if human[human_num] == 0 and cheese[cheese_num] > eat_time:
         human[human_num] = 1
         pill += 1
 print(pill)
