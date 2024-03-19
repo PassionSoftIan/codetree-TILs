37e53402009a7f613f1cea708f4241265077e6ff
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-03-20 02:13:48 +0900

    Title: 폭탄 해체 작업, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240320/폭탄 해체 작업/the-bomb-dismantling.py
+++ 240320/폭탄 해체 작업/the-bomb-dismantling.py
@@ -11,32 +11,46 @@
 
 1- 내 풀이
 
+1) 제한시간을 기준으로 내림차순 정렬한다.
+2) 가장 제한 시간이 높은 순서로 거꾸로 내려온다.
+3) 거꾸로 내려오며 해당 시간과 같거나 클 경우에 heapq에 넣는다.
+4) 아닌 경우가 생기면 그동안 모아놓은 heapq에서 최대 값을 찾아서 max_result에 더한다.
+5) max_result를 출력한다.
 
 '''
-
+import heapq
 
 N = int(input())
 
 bombs_info = []
 
-
+max_time = 0
 
 for i in range(N):
     score, time = map(int, input().split())
+    max_time = max(max_time, time)
     bombs_info.append([score, time])
 
-bombs_info.sort(key=lambda x : (-x[0], x[1]))
+bombs_info.sort(key=lambda x : -x[1])
 
-max_time = 0
+
 idx = 0
 
+scores = []
+
 max_result = 0
-while idx != N:
+result = 0
+while idx != N+1:
+    if idx == N:
+        if scores:
+            max_result += -heapq.heappop(scores)
+            break
     if bombs_info[idx][1] >= max_time:
-        max_result += bombs_info[idx][0]
-        max_time += 1
+        heapq.heappush(scores, -bombs_info[idx][0])
         idx += 1
     else:
-        idx += 1
+        if scores:
+            max_result += -heapq.heappop(scores)
+        max_time -= 1
 
 print(max_result)
