743d5bbddcf99f84fbb273177bebae38ab0b1b9a
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-03-21 14:51:56 +0900

    Title: 문자에 따른 명령 2, Time: 120ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240321/문자에 따른 명령 2/text-based-commands2.py
@@ -0,0 +1,40 @@
+'''
+1. 좌표 평면 0, 0에서 시작.
+2. 북쪽을 바라보고 있음.
+3. N개의 명령어가 주어짐.
+4. L은 반시계 90도 방향.
+5. R은 시계 90도 방향.
+6. F는 앞으로 이동.
+
+
+1- 내 풀이
+
+1) dy, dx를 [동, 남, 서, 북] 으로 생성.
+2) 초기 방향 direction = 3 (북)으로 생성.
+3) L일 경우 direction = (direction + 3) % 4
+4) R일 경우 direction = (direction + 1) % 4
+5) F일 경우 y = y + dy[direction], x = x + dx[direction]
+6) print(y, x)
+
+'''
+
+dy = [0, 1, 0, -1]
+dx = [1, 0, -1, 0]
+
+commands = list(map(str, input()))
+
+y, x = 0, 0
+
+direction = 3
+for command in commands:
+    if command == 'L':
+        direction = (direction + 3) % 4
+    
+    if command == 'R':
+        direction = (direction + 1) % 4
+    
+    if command == 'F':
+        y = y + dy[direction]
+        x = x + dx[direction]
+
+print(x, y)
