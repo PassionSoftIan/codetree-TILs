1f56cfabd670c20e69549fa823e0fd5e4cb9a555
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-04-24 16:45:12 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240424/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int G = 13;
+    double M = 0.165;
+
+    cout << fixed;
+    cout.precision(6);
+
+    cout << G << " * " << M << " = " << G * M;
+    return 0;
+}
