a4bbdef7779c4262d82213dde17cefcc8ce148e3
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-04-24 14:46:50 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240424/다양하게 출력/print-in-variety.cpp
+++ 240424/다양하게 출력/print-in-variety.cpp
@@ -2,9 +2,12 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    std::cout << "Total days in Year" << std::endl;
-    std::cout << 365 << std::endl;
-    std::cout << "Circumference rate" << std::endl;
+    std::cout << "Total days in Year";
+    std::cout << std::endl;
+    std::cout << 365;
+    std::cout << std::endl;
+    std::cout << "Circumference rate";
+    std::cout << std::endl;
     std::cout << "3.1415926535";
     return 0;
 }
