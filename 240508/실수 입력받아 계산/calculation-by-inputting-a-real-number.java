72bd6ef9c88576f1cc51d5aaab8bebae4c46ca77
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-05-08 11:40:49 +0900

    Title: 실수 입력받아 계산, Time: 171ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240508/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        double a, b;
+
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+
+        System.out.printf("%.2f", a+b);
+    }
+}
