67180b7d431cd81cb4b487df1f189d9c5cdc2649
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-05-08 10:59:21 +0900

    Title: 입력받아 계산 3, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240508/입력받아 계산 3/input-calculate-3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a, b;
+
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.print(a*b);
+
+    }
+}
