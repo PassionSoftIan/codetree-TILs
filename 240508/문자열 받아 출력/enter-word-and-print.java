be034778db1a406b7a1fa07cea16f527e756453f
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-05-08 12:25:13 +0900

    Title: 문자열 받아 출력, Time: 116ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240508/문자열 받아 출력/enter-word-and-print.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        String a;
+
+        Scanner sc = new Scanner(System.in);
+        a = sc.next();
+
+        System.out.print(a);
+    }
+}
