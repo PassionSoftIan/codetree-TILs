b4177b0c779ab5327389487b14afde255efaf8a7
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-08-01 12:57:49 +0900

    Title: 왔다 갔던 구역 2, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240801/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,26 @@
+N = int(input())
+
+arr = [0] * 202
+
+start = 101
+
+for i in range(N):
+    go, cmd = map(str, input().split())
+    go = int(go)
+
+    if cmd == 'R':
+        for j in range(go):
+            arr[start + j] += 1
+        start += go
+
+    if cmd == 'L':
+        for j in range(go):
+            arr[start - j] += 1
+        start -= go
+
+count = 0
+for i in range(len(arr)):
+    if arr[i] >= 2:
+        count += 1
+
+print(count)
