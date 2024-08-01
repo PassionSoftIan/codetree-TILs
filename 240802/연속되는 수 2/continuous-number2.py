14813ff4766312f4ef2240b214db1ed7c976923c
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-08-02 01:32:17 +0900

    Title: 연속되는 수 2, Time: 131ms, Memory: 25MB, Status: Passed - CodeTree

--- 240802/연속되는 수 2/continuous-number2.py
+++ 240802/연속되는 수 2/continuous-number2.py
@@ -2,15 +2,16 @@ N = int(input())
 
 arr = [int(input()) for _ in range(N)]
 
-max_count = 1
+max_count = 0
 
-count = 1
-for i in range(1, N):
-    if arr[i-1] == arr[i] and i != N-1:
+count = 0
+for i in range(N):
+    if i == 0 or arr[i-1] == arr[i]:
         count += 1
     
     else:
-        max_count = max(max_count, count)
         count = 1
+    
+    max_count = max(max_count, count)
 
 print(max_count)
