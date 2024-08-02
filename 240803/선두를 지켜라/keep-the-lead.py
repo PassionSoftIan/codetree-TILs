a7c8c3f0b958fce3f283d34c90e7be385634af5c
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-08-03 04:14:12 +0900

    Title: 선두를 지켜라, Time: 120ms, Memory: 40MB, Status: Wrong Answer - CodeTree

--- 240803/선두를 지켜라/keep-the-lead.py
+++ 240803/선두를 지켜라/keep-the-lead.py
@@ -19,24 +19,42 @@ for i in range(M):
     v, t = map(int, input().split())
 
     for j in range(t):
-        B[B_start + j] = A[B_start + j - 1] + v
+        B[B_start + j] = B[B_start + j - 1] + v
     
     B_start += t
 
 count = 0
+A_check = 0
+B_check = 0
+
+check = 1
+while True:
+    if A[check] > B[check] or A[check] < B[check]:
+        A_check = A[check]
+        B_check = B[check]
+        break
+    check += 1
+print()
+
 for i in range(2, 1000001):
     if A[i] == 0 or B[i] == 0:
         break
-    if A[1] > B[1]:
+    if A_check > B_check:
         if A[i] < B[i]:
             count += 1
+            A_check = A[i]
+            B_check = B[i]
         
-    elif A[1] < B[1]:
+    elif A_check < B_check:
         if A[i] > B[i]:
             count += 1
-    
-    elif A[1] == B[1]:
+            A_check = A[i]
+            B_check = B[i]
+
+    elif A_check == B_check:
         if A[i] > B[i] or A[i] < B[i]:
             count += 1
+            A_check = A[i]
+            B_check = B[i]
 
 print(count)
