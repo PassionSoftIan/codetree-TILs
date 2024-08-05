38f375d620b3f7851daec88f4c0f395195cff48a
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-08-06 04:15:30 +0900

    Title: 체크판위에서 2, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240806/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,16 @@
+N, M = map(int, input().split())
+
+arr = [list(map(str, input().split())) for _ in range(N)]
+
+count = 0
+for i in range(1, N-2):
+    for j in range(1, M-2):
+        if arr[0][0] == arr[i][j]:
+            continue
+        for k in range(i+1, N-1):
+            for l in range(j+1, M-1):
+                if arr[i][j] == arr[k][l]:
+                    continue
+                count += 1
+
+print(count)
