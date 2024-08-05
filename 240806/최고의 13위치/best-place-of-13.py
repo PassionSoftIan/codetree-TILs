335b3f5d9e0682a3e75628b23fd858112d742c1e
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-08-06 03:53:02 +0900

    Title: 최고의 13위치, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240806/최고의 13위치/best-place-of-13.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+max_count = 0
+
+for i in range(N):
+    for j in range(N-2):
+        count = 0
+        for k in range(3):
+            if arr[i][k] == 1:
+                count += 1
+        max_count = max(max_count, count)
+
+print(max_count)
