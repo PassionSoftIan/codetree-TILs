555cdab351b01d08f73234d383945bb8fa2da003
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-08-07 04:26:58 +0900

    Title: 특정 구간의 원소 평균값, Time: 120ms, Memory: 25MB, Status: Passed - CodeTree

--- 240807/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
+++ 240807/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -7,12 +7,14 @@ for i in range(N):
     for j in range(i, N):
         temp_sum = 0
         temp_count = 0
+        temp_arr = []
         for k in range(i, j+1):
             temp_sum += arr[k]
             temp_count += 1
+            temp_arr.append(arr[k])
         temp_avg = temp_sum / temp_count
 
-        if temp_avg in arr:
+        if temp_avg in temp_arr:
             count += 1
 
 print(count)
