001c797c818503ca7492ff89f0bc3e4f29ce64ee
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-08-07 03:54:20 +0900

    Title: G or H 3, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240807/G or H 3/G-or-H-3.py
+++ 240807/G or H 3/G-or-H-3.py
@@ -29,7 +29,7 @@ for i in range(N):
         arr[location] = 2
 
 max_count = 0
-for i in range(start, end - K + 3):
+for i in range(start, end - K):
     count = 0
     for j in range(K+1):
         count += arr[i + j]
