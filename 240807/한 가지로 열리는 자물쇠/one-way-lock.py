81b9d973253094bd9a92bba34887d7245bc1e502
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-08-07 17:06:51 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 258ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240807/한 가지로 열리는 자물쇠/one-way-lock.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+arr = list(map(int, input().split()))
+
+count = 0
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        for k in range(1, N+1):
+            if abs(arr[0] - i) <= 2 or abs(arr[1] - j) <= 2 or abs(arr[2] - k) <=2:
+                count += 1
+
+print(count)
