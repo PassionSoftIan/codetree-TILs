321be7f2912a14abb747a3330665c3b79b672fd8
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-08-13 19:18:48 +0900

    Title: 가장 많이 나온 쌍, Time: 115ms, Memory: 26MB, Status: Passed - CodeTree

--- 240813/가장 많이 나온 쌍/the-most-frequent-pair.py
+++ 240813/가장 많이 나온 쌍/the-most-frequent-pair.py
@@ -3,8 +3,8 @@ N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(M)]
 
 max_count = 0
-for i in range(1, N):
-    for j in range(1, N):
+for i in range(1, N+1):
+    for j in range(1, N+1):
         count = 0
         for a, b in arr:
             if (i == a and j == b) or (i == b and j == a):
