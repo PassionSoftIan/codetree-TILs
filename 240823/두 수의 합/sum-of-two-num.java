ebfc55c9999e4fbe741ddc18af35b10632be39d6
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-08-23 04:08:20 +0900

    Title: 두 수의 합, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240823/두 수의 합/sum-of-two-num.java
+++ 240823/두 수의 합/sum-of-two-num.java
@@ -3,41 +3,40 @@ import java.util.*;
 public class Main {
 
     public static int N;
-    public static int K;
+    public static long K;
 
-    public static int[] arr;
+    public static HashMap<Long, Long> check = new HashMap<>();
 
-    public static HashMap<Integer, Integer> map = new HashMap<>();
-
-    public static ArrayList<Integer> nums = new ArrayList<>();
+    public static ArrayList<Long> nums = new ArrayList<>();
 
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
         N = sc.nextInt();
-        K = sc.nextInt();
-
-        arr = new int[100001];
+        K = sc.nextLong();
 
         int result = 0;
 
         for (int i = 0; i < N; i++) {
-            int num = sc.nextInt();
-            arr[num] += 1;
+            long num = sc.nextLong();
+            if (check.containsKey(num)) {
+                check.put(num, check.get(num) + 1);
+            }
+            else {
+                check.put(num, 1L);
+            }
             nums.add(num);
-            map.put(num, K-num);
         }
 
         for (int i = 0; i < N; i++) {
-            int num = nums.get(i);
+            long num = nums.get(i);
+            long pair = K - num;
 
-            if (arr[num] > 0) {
-                if (map.containsKey(map.get(num))) {
-                    arr[num] -= 1;
-                    arr[map.get(num)] -= 1;
-                    result += 1;
-                }
+            if (check.containsKey(num) && check.containsKey(pair) && check.get(num) > 0 && check.get(pair) > 0) {
+                result += 1;
+                check.put(num, check.get(num) - 1);
+                check.put(pair, check.get(pair) - 1);
             }
         }
         System.out.print(result);
