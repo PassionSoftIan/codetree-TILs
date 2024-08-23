ef2217e9436bd9602911506b8d0f932dd050c13f
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-08-23 20:02:21 +0900

    Title: 두 수의 합, Time: 1012ms, Memory: 48MB, Status: Passed - CodeTree

--- 240823/두 수의 합/sum-of-two-num.java
+++ 240823/두 수의 합/sum-of-two-num.java
@@ -3,46 +3,37 @@ import java.util.*;
 public class Main {
 
     public static int N;
-    public static long K;
+    public static int K;
 
-    public static HashMap<Long, Long> check = new HashMap<>();
-
-    public static ArrayList<Long> nums = new ArrayList<>();
+    public static int[] arr = new int[100001];
+    public static HashMap<Integer, Integer> numCheck = new HashMap<>();
 
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
+
         Scanner sc = new Scanner(System.in);
 
         N = sc.nextInt();
-        K = sc.nextLong();
+        K = sc.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            arr[i] += sc.nextInt();
+        }
 
         int result = 0;
 
         for (int i = 0; i < N; i++) {
-            long num = sc.nextLong();
-            if (check.containsKey(num)) {
-                check.put(num, check.get(num) + 1);
+            int num = arr[i];
+            int pair = K - num;
+
+            if (numCheck.containsKey(pair)) {
+                result += numCheck.get(pair);
+            }
+
+            if (!numCheck.containsKey(num)) {
+                numCheck.put(num, 1);
             }
             else {
-                check.put(num, 1L);
-            }
-            nums.add(num);
-        }
-
-        for (int i = 0; i < N; i++) {
-            long num = nums.get(i);
-            long pair = K - num;
-
-            if (check.containsKey(num) && check.containsKey(pair) && check.get(num) > 0 && check.get(pair) > 0) {
-                if (num == pair) {
-                    long count = check.get(num);
-                    result += (count * (count - 1)) / 2;
-                    check.put(num, 0L);
-                } else {
-                    result += 1;
-                    check.put(num, check.get(num) - 1);
-                    check.put(pair, check.get(pair) - 1);
-                }
+                numCheck.put(num, numCheck.get(num) + 1);
             }
         }
         System.out.print(result);
