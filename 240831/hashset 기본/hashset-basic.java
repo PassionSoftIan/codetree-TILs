30315aa7e3a8d79d24a8621b5bd18288349ddd48
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-08-31 02:57:40 +0900

    Title: hashset 기본, Time: 1644ms, Memory: 51MB, Status: Passed - CodeTree

---
+++ 240831/hashset 기본/hashset-basic.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+
+    public static int N;
+
+    public static HashSet<Integer> set = new HashSet<>();
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            String cmd = sc.next();
+            
+            if (cmd.equals("add")) {
+                int num = sc.nextInt();
+                set.add(num);
+            }
+            else if (cmd.equals("remove")) {
+                int num = sc.nextInt();
+                set.remove(num);
+            }
+            else {
+                int num = sc.nextInt();
+                System.out.println(set.contains(num));
+            }
+        }
+    }
+}
