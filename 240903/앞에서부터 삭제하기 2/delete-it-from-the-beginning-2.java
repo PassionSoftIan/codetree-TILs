4f29bac91532eb6863e2b85d6d7580c3baf41c88
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-09-03 02:07:26 +0900

    Title: 앞에서부터 삭제하기 2, Time: 3184ms, Memory: 176MB, Status: Time Limit Exceed - CodeTree

---
+++ 240903/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class Main {
+
+    public static int N;
+    public static int[] arr;
+    public static PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+    public static int maxSum;
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+
+        arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < N; i++) {
+            maxSum += arr[i];
+        }
+
+        int K = 0;
+
+        float maxResult = 0;
+
+        while (K <= N-3) {
+            maxSum -= arr[K];
+
+            int tempSum = maxSum;
+
+            for (int i = K+1; i < N; i++) {
+                pq.add(arr[i]);
+            }
+
+            tempSum -= pq.poll();
+
+            float avg = (tempSum / (N - (K + 2)));
+
+            maxResult = Math.max(maxResult, avg);
+
+            K += 1;
+        }
+
+        System.out.printf("%.2f", maxResult);
+    }
+}
