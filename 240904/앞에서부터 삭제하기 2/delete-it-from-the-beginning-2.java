0b2365968a967429151791ab563c1bb6a14d964a
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-09-04 03:15:34 +0900

    Title: 앞에서부터 삭제하기 2, Time: 829ms, Memory: 38MB, Status: Wrong Answer - CodeTree

---
+++ 240904/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+
+    public static int N;
+    public static int[] arr;
+    public static PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+
+        arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int K = N-3;
+
+        pq.add(arr[N-1]);
+
+        int total = arr[N-1];
+
+        float maxResult = arr[N-1];
+
+        for (int i = N-2; i >= 1; i--) {
+            pq.add(arr[i]);
+
+            total += arr[i];
+
+            maxResult = Math.max(maxResult, (total - pq.peek()) / (pq.size() - 1));
+        }
+        System.out.printf("%.2f", maxResult);
+    }
+}
