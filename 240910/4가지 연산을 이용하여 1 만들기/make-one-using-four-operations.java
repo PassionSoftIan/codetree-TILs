6aa55f95027e5b18dc5067068d5798ff59b7f141
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-09-10 01:43:26 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240910/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.java
+++ 240910/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.java
@@ -12,10 +12,9 @@ class Pair {
 public class Main {
 
     public static int N;
-
     public static Queue<Pair> q = new LinkedList<>();
-
     public static int result = 0;
+    public static boolean[] visited; // 방문 여부를 추적할 배열
 
     public static int first(int num) {
         return num - 1;
@@ -40,7 +39,10 @@ public class Main {
     }
 
     public static void push(int num, int count) {
-        q.add(new Pair(num, count));
+        if (num >= 1 && num <= N && !visited[num]) {  // 범위 내의 숫자만 큐에 넣음
+            visited[num] = true; // 방문 처리
+            q.add(new Pair(num, count));
+        }
     }
 
     public static void BFS() {
@@ -57,22 +59,20 @@ public class Main {
             push(first(num), count+1);
             push(second(num), count+1);
 
-            if (third(num) != - 1) {
+            if (third(num) != -1) {
                 push(third(num), count + 1);
             }
 
-            if (fourth(num) != - 1) {
+            if (fourth(num) != -1) {
                 push(fourth(num), count + 1);
             }
         }
     }
-    
+
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-
         Scanner sc = new Scanner(System.in);
-
         N = sc.nextInt();
+        visited = new boolean[N + 1]; // 방문 배열 초기화
 
         push(N, 0);
         BFS();
