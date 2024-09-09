ee96ccfe6586967ac6b23e29363983c2a34ae9ff
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-09-09 17:28:26 +0900

    Title: 비를 피하기, Time: 423ms, Memory: 22MB, Status: Passed - CodeTree

--- 240909/비를 피하기/stay-out-of-rain.java
+++ 240909/비를 피하기/stay-out-of-rain.java
@@ -22,7 +22,7 @@ public class Main {
     public static int[][] info;
     public static boolean[][] visited;
 
-    public static Queue<Pair> q;
+    public static Queue<Pair> q = new LinkedList<>();
 
     public static ArrayList<Pair> coordinate = new ArrayList<>();
 
@@ -36,7 +36,7 @@ public class Main {
         q.add(new Pair(n, m, count));
     }
 
-    public static void BFS(int x, int y) {
+    public static void BFS() {
 
         while (!q.isEmpty()) {
             Pair pair = q.poll();
@@ -44,11 +44,9 @@ public class Main {
             int m = pair.m;
             int count = pair.count;
 
-            if (arr[n][m] == 3) {
-                info[x][y] = count;
-                return;
+            if (arr[n][m] == 2) {
+                info[n][m] = count;
             }
-
 
             for (int i = 0; i < 4; i++) {
                 int ny = n + dy[i];
@@ -71,6 +69,7 @@ public class Main {
         M = sc.nextInt();
 
         arr = new int[N][N];
+        visited = new boolean[N][N];
         info = new int[N][N];
 
         for (int i = 0; i < N; i++) {
@@ -80,6 +79,9 @@ public class Main {
 
                 if (num == 2) {
                     info[i][j] = -1;
+                }
+
+                if (num == 3) {
                     coordinate.add(new Pair(i, j, 0));
                 }
             }
@@ -91,12 +93,11 @@ public class Main {
             int m = pair.m;
             int count = pair.count;
 
-            visited = new boolean[N][N];
-            q = new LinkedList<>();
-
             push(n, m, count);
-            BFS(n, m);
         }
+
+        BFS();
+
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 System.out.print(info[i][j] + " ");
@@ -105,3 +106,115 @@ public class Main {
         }
     }
 }
+
+
+
+// 각 사람에 대한 BFS 코드
+
+// import java.util.*;
+
+// class Pair {
+//     int n, m, count;
+
+//     public Pair(int n, int m, int count) {
+//         this.n = n;
+//         this.m = m;
+//         this.count = count;
+//     }
+// }
+
+// public class Main {
+//     public static int[] dy = {0, 1, 0, -1};
+//     public static int[] dx = {1, 0, -1, 0};
+
+//     public static int N;
+//     public static int H;
+//     public static int M;
+
+//     public static int[][] arr;
+//     public static int[][] info;
+//     public static boolean[][] visited;
+
+//     public static Queue<Pair> q;
+
+//     public static ArrayList<Pair> coordinate = new ArrayList<>();
+
+//     public static boolean canGo(int ny, int nx) {
+//         return 0 <= ny && ny < N && 0 <= nx && nx < N && !visited[ny][nx] && arr[ny][nx] != 1;
+//     }
+
+    
+//     public static void push(int n, int m, int count) {
+//         visited[n][m] = true;
+//         q.add(new Pair(n, m, count));
+//     }
+
+//     public static void BFS(int x, int y) {
+
+//         while (!q.isEmpty()) {
+//             Pair pair = q.poll();
+//             int n = pair.n;
+//             int m = pair.m;
+//             int count = pair.count;
+
+//             if (arr[n][m] == 3) {
+//                 info[x][y] = count;
+//                 return;
+//             }
+
+
+//             for (int i = 0; i < 4; i++) {
+//                 int ny = n + dy[i];
+//                 int nx = m + dx[i];
+
+//                 if (canGo(ny, nx)) {
+//                     push(ny, nx, count+1);
+//                 }
+//             }
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         // 여기에 코드를 작성해주세요.
+
+//         Scanner sc = new Scanner(System.in);
+
+//         N = sc.nextInt();
+//         H = sc.nextInt();
+//         M = sc.nextInt();
+
+//         arr = new int[N][N];
+//         info = new int[N][N];
+
+//         for (int i = 0; i < N; i++) {
+//             for (int j = 0; j < N; j++) {
+//                 int num = sc.nextInt();
+//                 arr[i][j] = num;
+
+//                 if (num == 2) {
+//                     info[i][j] = -1;
+//                     coordinate.add(new Pair(i, j, 0));
+//                 }
+//             }
+//         }
+
+//         for (int i = 0; i < coordinate.size(); i++) {
+//             Pair pair = coordinate.get(i);
+//             int n = pair.n;
+//             int m = pair.m;
+//             int count = pair.count;
+
+//             visited = new boolean[N][N];
+//             q = new LinkedList<>();
+
+//             push(n, m, count);
+//             BFS(n, m);
+//         }
+//         for (int i = 0; i < N; i++) {
+//             for (int j = 0; j < N; j++) {
+//                 System.out.print(info[i][j] + " ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
