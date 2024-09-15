418fa9ffa6a1aa6ff4e05ff85ed4efe75296083e
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-09-15 23:11:39 +0900

    Title: 피보나치 수, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240915/피보나치 수/fibonacci-number.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+
+    public static int N;
+
+    public static int[] dp = new int[50];
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        System.out.println(dp[N]);
+
+    }
+}
