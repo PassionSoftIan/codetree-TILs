94a9f166230b65d450744236795c49db87d45735
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-09-16 11:37:49 +0900

    Title: 사각형 채우기 3, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

--- 240916/사각형 채우기 3/rectangle-fill-3.java
+++ 240916/사각형 채우기 3/rectangle-fill-3.java
@@ -1,27 +1,32 @@
 import java.util.*;
+import java.math.BigInteger;
 
 public class Main {
 
     public static int N;
 
-    public static long[] dp;
+    public static BigInteger[] dp;
 
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
 
         Scanner sc = new Scanner(System.in);
 
         N = sc.nextInt();
 
-        dp = new long[1001];
+        dp = new BigInteger[1001];
 
-        dp[1] = 2;
-        dp[2] = 7;
-        dp[3] = 22;
-        dp[4] = 71;
+        dp[1] = new BigInteger("2");
+        dp[2] = new BigInteger("7");
+        dp[3] = new BigInteger("22");
+        dp[4] = new BigInteger("71");
+        
+        BigInteger MOD = new BigInteger("1000000007");
         
         for (int i = 4; i <= N; i++) {
-            dp[i] = ((dp[i-1] * 3) + dp[i-2] - dp[i-3]) % 1000000007;
+            dp[i] = dp[i-1].multiply(new BigInteger("3"))
+                           .add(dp[i-2])
+                           .subtract(dp[i-3])
+                           .mod(MOD);
         }
 
         System.out.print(dp[N]);
