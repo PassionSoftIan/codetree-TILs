f65e29aa5e223f8c6f6a458a91955a5caef8ff57
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-09-20 15:33:29 +0900

    Title: 정수 사각형 최소 합, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240920/정수 사각형 최소 합/minimum-sum-path-in-square.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+
+    public static int N;
+
+    public static int[][] arr;
+    public static int[][] dp;
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+
+        arr = new int[N][N];
+        dp = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        dp[0][N-1] = arr[0][N-1];
+
+        for (int i = 1; i < N; i++) {
+            dp[i][2] = dp[i-1][2] + arr[i][2];
+        }
+
+        for (int i = N-2; i >= 0; i--) {
+            dp[0][i] = dp[0][i+1] + arr[0][i];
+        }
+
+        for (int i = 1; i < N; i++) {
+            for (int j = N-2; j >= 0; j--) {
+                dp[i][j] = Math.min(dp[i-1][j], dp[i][j+1]) + arr[i][j];
+            }
+        }
+
+        System.out.print(dp[N-1][0]);
+    }
+}
